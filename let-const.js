abcdd16023425ab645aab81693d1ea06db3ba3b3
Robin Jacob Plumley <jrplumley@gmail.com>
2023-06-13 18:13:24 -0400

    added

---
+++ let-const.js
@@ -0,0 +1,21 @@
+// ES5 constant
+// var obj = {}
+// Object.defineProperties(obj,'PI', {
+//     value : 3.14,
+//     writable: false,
+//     enumerable: true,
+//     configurable: false
+// });
+
+// ES2015 Constant
+// const PI = 3.14;
+
+// Quiz
+// - What is the difference between ***var*** and ***let***?
+// var is a declaration that is function scoped and is able to be reassigned and redeclared. Let is block scoped and can only be reassigned but not redeclared
+// - What is the difference between **var** and ***const***?
+// var is a declaration that is function scoped and is able to be reassiged and redeclared. Const is block scoped and can't be redeclared or reassigned but is mutatable.
+// - What is the difference between **let** and **const**?
+// let and const are both block scoped and both unable to be redeclared. But the main difference is const can't be reassigned where as let can.
+// - What is hoisting?
+// variables declared with the var declaration are hoisted to the top of the scope and declared before initilliazation but not defined.
